4cf498b68fb32d8706d0f33c201333541f42cbdf
ZackYu <624302105@qq.com>
2017-11-15 15:07:04 +0800

    v 1.0.0
    A new start!

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png">
+    <div>
+      <el-button @click="startHacking">Start</el-button>
+    </div> -->
+    <navbar></navbar>
+    <div id="footer">
+      <span>Copyright © 2017 shuZack. All rights reserved.</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import navbar from "./components/navbar.vue";
+export default {
+  components: {
+    navbar
+  },
+  methods: {
+    // startHacking() {
+    //   this.$notify({
+    //     title: "It works!",
+    //     type: "success",
+    //     message:
+    //       "We've laid the ground work for you. It's time for you to build something epic!",
+    //     duration: 5000
+    //   });
+    // }
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0px;
+  padding: 0px;
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+}
+#footer {
+    background: #409EFF;
+    padding: 10px 0 6px 0;
+    width: 100%;
+    text-align: center;
+    color: white;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="navbar">
+    <el-menu :default-active="activeIndex2" mode="horizontal" background-color="#409EFF" text-color="#fff" active-text-color="#ffd04b">
+      <el-menu-item index="1">主页</el-menu-item>
+      <el-menu-item index="2">学习资料</el-menu-item>
+      <el-menu-item index="3">电影</el-menu-item>
+      <el-menu-item index="4">音乐</el-menu-item>
+      <el-menu-item index="5">游戏</el-menu-item>
+      <el-menu-item index="6">其他</el-menu-item>
+      <el-input placeholder="请键入查找内容" v-model="searchContent">{{searchContent}}
+        <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
+      </el-input>
+      <h1 id="brand">上海大学PT站</h1>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+      activeIndex2: "1",
+      searchContent:''
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    search(){
+      this.$message('正在为你查找'+searchContent);
+  }
+}
+}
+</script>
+
+<style>
+.el-menu-item {
+  width: 120px;
+  text-align: center;
+  font-size: 20px;
+}
+.el-input {
+  width: 400px;
+  margin-top: 10px;
+  float: right;
+  margin-right: 350px;
+}
+#brand {
+  margin-top: -40px;
+  float: right;
+  margin-right:20px;
+  color: white;
+  z-index: 1;
+}
+</style>
